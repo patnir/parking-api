25eb58c1fcbeae13a1ecb8aee12e8f56249db030
Rahul Patni <rahul20patni@gmail.com>
2021-03-21 10:56:03 -0700

    making apis more rest-like

--- delete.js
+++ delete.js
@@ -2,12 +2,10 @@ import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
-  const license = data.license;
   const params = {
     TableName: process.env.tableName,
     Key: {
-      license: license,
+      license: event.pathParameters.id,
     },
   };
 
--- get.js
+++ get.js
@@ -2,13 +2,11 @@ import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-  const data = JSON.parse(event.body);
-  const license = data.license;
   const params = {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
     Key: {
-      license: license, // The id of the author
+      license: event.pathParameters.id, // The id of the author
     },
   };
 
